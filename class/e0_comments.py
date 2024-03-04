7de709cc82aed4d58b2cb3f50e66cb5a2b10c5f7
ShantalV0 <shan.vel.delao@gmail.com>
2024-03-04 12:11:01 -0600

    ex0 and ex1 done

--- class/e0_comments.py
+++ class/e0_comments.py
@@ -1,10 +1,21 @@
-a = 5
-
+a = 5  # (inline comment) this is an instance of an object
+"""
+This is a block comment
+We can add a lot of lines
+"""
 
 def f(x):
-    return x * x
+    """
+    This is a docstring
+    A docstring explains what a function docs
+    :param x:
+    :return:
+    """
+    return x * x  # Returns the square of x
 
 
 for item in 'python':
+    # An indented comment
     if item == 'p':
+        # Another indetented comment
         print('Found!')
--- class/e1_helloworld.py
+++ class/e1_helloworld.py
@@ -2,19 +2,28 @@
 
 # "Once Charles Dickens said: "It was the best of times, it was the worst of times""
 
-
+# print('Once Charles Dickens said: "It was the best of times, it was the worst of times"')
+# print("Once Charles Dickens said: \"It was the best of times, it was the worst of times\"")
 
 # 5    7    9    11
 
+#print(5, 7, 9, 11, sep='    ')
+#print(5, 7, 9, 11, sep='\t')
 
 """
 Actuarial
 Science
 """
 
+#print('Actuarial')
+#print('Science')
+#print('Actuarial', "science", sep='\n')
 
 # With just three '-' print out five '-'
 
+#print('-','-','-', sep='-')
+
 
 # Hello world (this is the first program that all programmers do)
+#print("Hello word")
 
