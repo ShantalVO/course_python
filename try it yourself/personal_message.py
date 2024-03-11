df06d9bbbe601d41170fd263e1be977555481ac6
ShantalVO <124198760+ShantalVO@users.noreply.github.com>
2024-03-11 00:08:46 -0600

    Add files via upload

---
+++ try it yourself/personal_message.py
@@ -0,0 +1,8 @@
+# Pregunatar al usuario por su nombre y asignarlo en una variable
+
+name = input("¿Cuál es tu nombre?")
+
+# Imprimir el mensaje con algo simple
+
+personal_message = f"Hola {name}, ¿Cuál es la operación contraria a un logaritmo natural?"
+print(personal_message)
