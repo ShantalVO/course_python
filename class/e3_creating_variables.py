9818dfc0f2b8cd99e4e4058719df90fbf321bba1
ShantalV0 <shan.vel.delao@gmail.com>
2024-03-10 23:26:06 -0600

    exercice 3 done

---
+++ class/e3_creating_variables.py
@@ -0,0 +1,19 @@
+# Creando variables
+
+my_string = "Hola a todos"
+my_int = 10
+my_float = 1.11
+my_list = ["computadora" , "mause", "teclado"]
+my_tuple = ("a" , "b" , "c")
+my_set = {"amarillo" , "morado" , "cafe"}
+my_dictionary = {"Raza": "Schnauzer", "Edad": 1}
+my_complex = 5j
+
+# Imprimir las variables en orden
+print(f"List : {my_list}")
+print(f"Integer: {my_int}")
+print(f"Tuple: {my_tuple}")
+print(f"Float: {my_float}")
+print(f"String: {my_string}")
+print(f"Dictionary: {my_dictionary}")
+print(f"Set: {my_set}")
