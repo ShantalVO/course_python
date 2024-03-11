8f453508873da13020f8f388648ef617ce51421e
ShantalV0 <shan.vel.delao@gmail.com>
2024-03-10 23:51:34 -0600

    exercice 2 done

---
+++ try it yourself/simple messages.py
@@ -0,0 +1,8 @@
+# Asignando un mensaje a una variable e imprimirla
+
+message = "Esto es el mensaje de las 10 de la mañana"
+print(message)
+
+# Cambiando el valor de la variable e imprimir el nuevo mensaje
+message = "Este es el nuevo mensaje de las 11 de la mañana"
+print(message)
