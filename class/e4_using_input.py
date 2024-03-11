a459c0185e7c6a9b36a11756d3e4e672fc759ad6
ShantalV0 <shan.vel.delao@gmail.com>
2024-03-10 23:36:41 -0600

    exercice 4 done

---
+++ class/e4_using_input.py
@@ -0,0 +1,7 @@
+# Prompt the name and store it in the variable 'my_name?
+
+my_name = input("¿Cuál es tu nombre?")
+
+# Imprimir el nombre almacenado en la variable
+
+print(f"El nombre es: {my_name}")
